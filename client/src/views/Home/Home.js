ce7d09981c024107a82e227abf496f1dc2765177
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-12-21 15:15:18 +0530

    create small changes in css and create basic dessign of  home page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
-
+import './Home.css';
 function Home() {
   return (
-    <div>Home</div>
+   <>
+   <div className='container'>
+    <div className='head-title shadow-lg p-3 bg-body rounded mt-4'>
+      <h3 className='text-center'>हम सभी का एक छोटा सा प्रयास एक बड़ा बदलाव ला सकता है।🙏</h3>
+    </div>
+    <div className='row'>
+      <div className='col-md-6'>
+            hell0
+      </div>
+      <div className='col-md-6'>
+            hello
+      </div>
+    </div>
+   </div>
+   </>
   )
 }
 
