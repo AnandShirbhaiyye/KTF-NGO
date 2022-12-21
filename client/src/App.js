a4a8db2dc5c18edee4bd07eedc36c066bdd75111
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-12-21 12:20:46 +0530

    feat: added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Register from './views/Register/Register';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./views/Login/Login";
+import Register from "./views/Register/Register";
+import Home from "./views/Home/Home";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <div className='container'>
-      <Register/>
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
